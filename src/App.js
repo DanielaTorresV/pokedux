e8344a54bb001fc8d34b6585a6fa378636596089
Daniela <dannytorres0211@gmail.com>
2022-09-09 21:52:09 -0500

    Connect redux with app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
+import { useEffect } from "react";
+import { connect } from "react-redux";
 import { Col } from "antd";
 import Searcher from "./components/Searcher";
 import PokemonList from "./components/PokemonList";
-import logo from "./statics/logo.svg";
+import { setPokemons as setPokemonsActions } from "./actions";
 import { getPokemon } from "./api";
-import { useEffect, useState } from "react";
+import logo from "./statics/logo.svg";
 import "./App.css";
 
-function App() {
-  const [pokemons, setPokemons] = useState([]);
-
+function App({ pokemons, setPokemons }) {
   useEffect(() => {
     const fetchPokemons = async () => {
       const pokemonsRes = await getPokemon();
@@ -30,4 +30,12 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  pokemons: state.pokemons,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setPokemons: (value) => dispatch(setPokemonsActions(value)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
